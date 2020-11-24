11537f0bb06620cf1c5e6bc1f8a9a618f1c825d6
Sourabh Shegane <sgsourabh30@gmail.com>
2020-11-24 19:34:04 +0530

    Created MainViewModel.kt and PlaybackStateCompatExtension.kt

--- app/src/main/java/com/sourabhcodes/spotifyclone/di/AppModule.kt
+++ app/src/main/java/com/sourabhcodes/spotifyclone/di/AppModule.kt
@@ -5,6 +5,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.sourabhcodes.spotifyclone.R
+import com.sourabhcodes.spotifyclone.exoplayer.MusicServiceConnection
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,6 +17,11 @@ import javax.inject.Singleton
 @InstallIn(ApplicationComponent::class)
 object AppModule {
 
+    @Singleton
+    fun provideMusicServiceConnection(
+        @ApplicationContext context: Context,
+
+    ) = MusicServiceConnection(context)
     @Singleton
     @Provides
     fun provideGlideInstance(
--- app/src/main/java/com/sourabhcodes/spotifyclone/ui/MainActivity.kt
+++ app/src/main/java/com/sourabhcodes/spotifyclone/ui/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.sourabhcodes.spotifyclone
+package com.sourabhcodes.spotifyclone.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.RequestManager
+import com.sourabhcodes.spotifyclone.R
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
